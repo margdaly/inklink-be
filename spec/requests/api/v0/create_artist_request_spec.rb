4c9275a792c530d492f639c2eb0d7df6dd63225a
Brian Guthrie <takingcareofthedeadfish@gmail.com>
2023-06-03 09:51:56 -0600

    Test: create test for post artist

---
+++ spec/requests/api/v0/create_artist_request_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe 'Artists API', type: :request do
+  describe 'POST /api/v0/artists' do
+    it 'creates an artist when all attributes are present' do
+      artist_params = ({ "name": "Bob Ross",
+                        "username": "bobrossrules@gmail.com",
+                        "password_digest": "password",
+                        "styles": ["American Traditional", "Watercolor"],
+                        "pricing": "$",
+                        "contact_info": "666-867-5309"
+                        })
+
+      headers = { CONTENT_TYPE => "application/json" }
+
+      post '/api/v0/artists', headers: headers, params: JSON.generate(artist: artist_params)
+
+      created_artist = Artist.last
+
+      expect(response).to be_successful
+      expect(response.status).to eq(201)
+      expect(created_artist.name).to eq(artist_params[:name])
+      expect(created_artist.username).to eq(artist_params[:username])
+      expect(created_artist.password_digest).to eq(artist_params[:password_digest])
+      expect(created_artist.styles).to eq(artist_params[:styles])
+      expect(created_artist.pricing).to eq(artist_params[:pricing])
+      expect(created_artist.contact_info).to eq(artist_params[:name])
+    end
+  end
+end
