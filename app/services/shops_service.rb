01552702759c06cf67f3b38f98bf41645cd57d8c
Brian Guthrie <takingcareofthedeadfish@gmail.com>
2023-06-03 17:50:26 -0600

    feat: Add methods to shops service

---
+++ app/services/shops_service.rb
@@ -0,0 +1,17 @@
+class ShopsService
+  def nearby_shops(location)
+    get_url("/v3/places/search?categories=11075&near=#{location}")
+  end
+
+  def get_url(url)
+    response = conn.get(url)
+    JSON.parse(response.body, symbolize_names: true)  
+  end
+
+  def conn 
+    Faraday.new(url: "https://api.foursquare.com") do |f|
+      require 'pry'; binding.pry
+      f.headers['Authorization'] = ENV['Authorization']
+    end
+  end
+end
