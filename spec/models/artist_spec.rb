f21dfe497bc5734360ea1cb1b786e6adf94b0d0d
Brian Guthrie <takingcareofthedeadfish@gmail.com>
2023-06-02 18:00:27 -0600

    Test: add tests for artist model

--- spec/models/artist_spec.rb
+++ spec/models/artist_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Artist, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it  { should validate_presence_of(:name) }
+    it  { should validate_presence_of(:username) }
+    it  { should validate_presence_of(:password_digest) }
+    it  { should validate_presence_of(:styles) }
+    it  { should validate_presence_of(:pricing) }
+    it  { should validate_presence_of(:contact_info) }
+  end
 end
