b7c0429d2dfc7969a913f0c9c3b1f8299bdcbd02
Brian Guthrie <takingcareofthedeadfish@gmail.com>
2023-06-02 18:17:00 -0600

    fix: update model and test for artist

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,3 +1,3 @@
 class Artist < ApplicationRecord
-  validates_presence_of :name, :username, :password_digest, :styles, :pricing, :contact_info
+  validates_presence_of :name, :username, :password_digest#, :styles, :pricing, :contact_info
 end
--- spec/models/artist_spec.rb
+++ spec/models/artist_spec.rb
@@ -5,8 +5,8 @@ RSpec.describe Artist, type: :model do
     it  { should validate_presence_of(:name) }
     it  { should validate_presence_of(:username) }
     it  { should validate_presence_of(:password_digest) }
-    it  { should validate_presence_of(:styles) }
-    it  { should validate_presence_of(:pricing) }
-    it  { should validate_presence_of(:contact_info) }
+    # it  { should validate_presence_of(:styles) }
+    # it  { should validate_presence_of(:pricing) }
+    # it  { should validate_presence_of(:contact_info) }
   end
 end
