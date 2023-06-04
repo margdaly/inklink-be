ff4842111b5bfb67642b5e2c1f9ac19165fe569a
Brian Guthrie <takingcareofthedeadfish@gmail.com>
2023-06-04 10:34:41 -0600

    test: First test for shops facade

---
+++ spec/facades/shops_facade_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe ShopsFacade do
+  describe 'exists' do
+    it 'really does exists' do
+      shops_facade = ShopsFacade.new
+
+      expect(shops_facade).to be_a(ShopsFacade)
+    end
+  end
+
+  describe 'nearby_shops' do
+    it 'returns an array of shops', :vcr do
+      shops_facade = ShopsFacade.new
+
+      expect(shops_facade.nearby_shops("colorado springs")).to be_a(Array)
+      expect(shops_facade.nearby_shops("colorado springs").to all(be_a(Shop)))
+    end
+  end
+end
