19e97914bafa9f9d8e362b92d81ac9310658ddbf
Brian Guthrie <takingcareofthedeadfish@gmail.com>
2023-06-06 11:54:50 -0600

    refactor: Update faker and routes

--- spec/factories/artists.rb
+++ spec/factories/artists.rb
@@ -5,6 +5,6 @@ FactoryBot.define do
     password { Faker::Internet.password }
     password_confirmation { password }
     styles { ["Watercolor", "American Traditional", "Geometric", "Script", "Black and Gray", "Realism", "Tribal", "Chicano", "Irezumi", "New School"].sample(2) }
-    pricing { "$" + Faker::Number.between(from: 10, to: 1000).to_s } 
+    pricing { ["$", "$$", "$$$"].sample } 
   end
 end
